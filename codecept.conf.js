1ece0c74269421f9d88750f4b0ff520a34825ebb
Yabgonz0310 <yabingonzaga0310@gmail.com>
2025-04-11 17:39:39 -0600

    Prueba simular usr en youtube

--- codecept.conf.js
+++ codecept.conf.js
@@ -15,6 +15,16 @@ exports.config = {
       browser: "chromium",
       url: "http://localhost",
       show: true,
+      chromium: {
+
+        userDataDir: './user-data-dir', // <-- esto guarda cookies y storage
+    
+        args: ['--start-maximized']
+    
+      },
+    
+      userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+     
     },
   },
   include: {
@@ -22,7 +32,7 @@ exports.config = {
   },
   gherkin: {
     features: "./features/*.feature",
-    steps: ["./steps/musicaSteps.js"],
+    steps: ["./steps/loginSteps.js","./steps/musicaSteps.js"],
   },
   name: "FrameworkYGL",
 };
