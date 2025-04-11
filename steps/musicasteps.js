1c4fc902c8e1e3c77c864cd7a5008d19a5fa980a
Yabgonz0310 <yabingonzaga0310@gmail.com>
2025-04-11 12:46:19 -0600

    Reproducción de video

--- steps/musicasteps.js
+++ steps/musicasteps.js
@@ -1,22 +1,23 @@
 const { I } = inject();
 
-Given("Estoy en la pagina principal", () => {
+Given("Estoy en la pagina principal de videos", () => {
   I.amOnPage("https://www.youtube.com/");
   I.wait(10);
 });
 
-When("Voy a la pagina de login", () => {
-  I.waitForElement('//a[text()="Login"]', 6);
-  I.click('//a[text()="Login"]');
+When("Capturo el nombre de la cancion", () => {
+  I.fillField("[placeholder="Buscar"]","YNLPA")
+  //I.waitForElement('[placeholder="Buscar"]', 5);
+  I.click('//button[@class="ytSearchboxComponentSearchButton"]');
 });
 
-When("Escribo nombre de usuario y contrasena a la pagina de login", () => {
+When("Selecciono el resultado de la busqueda", () => {
   I.wait(5);
-  I.fillField("#username", "yabingonzaga0310@gmail.com");
-  I.fillField("##password", "Ejemplo0920#");
+  I.clickLink('//img[@src="https://i.ytimg.com/vi/tm7-LlUc7uc/hq720.jpg?sqp=-oaymwEnCNAFEJQDSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLBJ9d95pMLsM9EUesX2XULIhNld4g"]')
+  //I.fillField("##password", "Ejemplo0920#");
   I.wait(2)
 });
 
-Then("Se visualiza link de ayuda", () => {
-  I.seeElement('//a[text()="Need help logging in?"]', 10);
+Then("Se visualiza el video", () => {
+  I.seeElement('src="blob:https://www.youtube.com/c8ecbac0-f5fc-4ccc-b74a-bd5a8b85128d"');
 });
